0555441dadb8046a4a4b2b0a181a89b4ae5b8b81
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 23:51:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deepestChild() {
 function increaseRankBy(n) {
   const list = document.querySelectorAll('.ranked-list li');
   for (let i = 0; i < list.length; i++) {
-    list[i].innerHTML = (i + 1).toString();
+    return list[i].innerHTML = (i + 1).toString();
   }
 }
